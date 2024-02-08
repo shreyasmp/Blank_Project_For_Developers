8acf254022769a119577347f5c605d4cfa2008d6
Shreyas Muthkur <shreyasspidy@gmail.com>
2024-02-08 03:28:07 -0500

    Blank buildable project for development with latest dependencies.

---
+++ app/src/main/java/com/shreyasmp/blankproject/ui/theme/Theme.kt
@@ -0,0 +1,37 @@
+package com.shreyasmp.blankproject.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+
+private val DarkColorScheme = darkColors(
+    primary = Purple80,
+    primaryVariant = Purple80,
+    secondary = PurpleGrey80
+)
+
+private val LightColorScheme = lightColors(
+    primary = Purple40,
+    primaryVariant = Purple80,
+    secondary = PurpleGrey40
+)
+
+@Composable
+fun BlankProjectTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) {
+        DarkColorScheme
+    } else {
+        LightColorScheme
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        content = content
+    )
+}
